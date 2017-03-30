d07b2d0b43c5f4b1202df12a77cf87075b9683e1
Oscar Corredor <oskar132@gmail.com>
2017-03-30 09:04:16 +0200

    new CSV data source, updated paticles with blackhole

--- resources/CSVtoJSONConverter/converter.py
+++ resources/CSVtoJSONConverter/converter.py
@@ -7,12 +7,18 @@ f = open(sys.argv[1], "rt")
 outfile = open('stars.js', 'w')
 reader = csv.DictReader(f)
 stars = []
-
+print("Processing rows")
+i = 0
 for row in reader:
+    print("Processing row "+str(i))
     star = Star(row)
     stars.append(star)
     json.dump(star.__dict__, outfile)
+    i = i+1
+    # if i==10000:
+    #     break
 
+print("Finished")
 
 f.close()
 outfile.close()
--- resources/CSVtoJSONConverter/star.py
+++ resources/CSVtoJSONConverter/star.py
@@ -1,8 +1,8 @@
 class Star:
     "The object representation of a star in our simulation"
     def __init__(self, data):
-        self.id = data["id"]
-        self.x = data["x"]
-        self.y = data["y"]
-        self.z = data["z"]
+        # self.id = data["id"]
+        self.x = "{0:.6f}".format(float(data["x"])/100000000000000)
+        self.y = "{0:.6f}".format(float(data["y"])/100000000000000)
+        self.z = "{0:.6f}".format(float(data["z"])/100000000000000)
         
