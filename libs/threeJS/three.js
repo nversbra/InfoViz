9a354c92cc5615a7dc1e2366d23154947fbeadfe
Nassim <nversbra@icloud.com>
2017-04-27 08:40:21 +0200

    added visualisation for camera positions

--- libs/threeJS/three.js
+++ libs/threeJS/three.js
@@ -40802,7 +40802,166 @@
 
 	}();
 
-	/**
+	//-------------------------
+
+    function CameraViz( camera ) {
+
+        var geometry = new BufferGeometry();
+        var material = new LineBasicMaterial( { color: 0xffffff, vertexColors: FaceColors } );
+
+        var vertices = [];
+        var colors = [];
+
+        var pointMap = {};
+
+        // colors
+
+        var colorFrustum = new Color( 0xffaa00 );
+        var colorCone = new Color( 0xff0000 );
+        var colorUp = new Color( 0x00aaff );
+        var colorTarget = new Color( 0xffffff );
+        var colorCross = new Color( 0x333333 );
+
+        // near
+
+        addLine( "n1", "n2", colorFrustum );
+        addLine( "n2", "n4", colorFrustum );
+        addLine( "n4", "n3", colorFrustum );
+        addLine( "n3", "n1", colorFrustum );
+
+
+
+        // sides
+
+        addLine( "n1", "f1", colorFrustum );
+        addLine( "n2", "f2", colorFrustum );
+        addLine( "n3", "f3", colorFrustum );
+        addLine( "n4", "f4", colorFrustum );
+
+        // cone
+
+        addLine( "p", "n1", colorCone );
+        addLine( "p", "n2", colorCone );
+        addLine( "p", "n3", colorCone );
+        addLine( "p", "n4", colorCone );
+
+        // up
+
+        addLine( "u1", "u2", colorUp );
+        addLine( "u2", "u3", colorUp );
+        addLine( "u3", "u1", colorUp );
+
+
+        function addLine( a, b, color ) {
+
+            addPoint( a, color );
+            addPoint( b, color );
+
+        }
+
+        function addPoint( id, color ) {
+
+            vertices.push( 0, 0, 0 );
+            colors.push( color.r, color.g, color.b );
+
+            if ( pointMap[ id ] === undefined ) {
+
+                pointMap[ id ] = [];
+
+            }
+
+            pointMap[ id ].push( ( vertices.length / 3 ) - 1 );
+
+        }
+
+        geometry.addAttribute( 'position', new Float32BufferAttribute( vertices, 3 ) );
+        geometry.addAttribute( 'color', new Float32BufferAttribute( colors, 3 ) );
+
+        LineSegments.call( this, geometry, material );
+
+        this.camera = camera;
+        if ( this.camera.updateProjectionMatrix ) this.camera.updateProjectionMatrix();
+
+        this.matrix = camera.matrixWorld;
+        this.matrixAutoUpdate = false;
+
+        this.pointMap = pointMap;
+
+        this.update();
+
+    }
+
+    CameraViz.prototype = Object.create( LineSegments.prototype );
+    CameraViz.prototype.constructor = CameraViz;
+
+    CameraViz.prototype.update = function () {
+
+        var geometry, pointMap;
+
+        var vector = new Vector3();
+        var camera = new Camera();
+
+        function setPoint( point, x, y, z ) {
+
+            vector.set( x, y, z ).unproject( camera );
+
+            var points = pointMap[ point ];
+
+            if ( points !== undefined ) {
+
+                var position = geometry.getAttribute( 'position' );
+
+                for ( var i = 0, l = points.length; i < l; i ++ ) {
+
+                    position.setXYZ( points[ i ], vector.x, vector.y, vector.z );
+
+                }
+
+            }
+
+        }
+
+        return function update() {
+
+            geometry = this.geometry;
+            pointMap = this.pointMap;
+
+            var w = 1, h = 1;
+
+            // we need just camera projection matrix
+            // world matrix must be identity
+
+            camera.projectionMatrix.copy( this.camera.projectionMatrix );
+
+            // center / target
+
+            setPoint( "c", 0, 0, - 1 );
+            setPoint( "t", 0, 0,  1 );
+
+            // near
+
+            setPoint( "n1", - w, - h, - 1 );
+            setPoint( "n2",   w, - h, - 1 );
+            setPoint( "n3", - w,   h, - 1 );
+            setPoint( "n4",   w,   h, - 1 );
+
+            // up
+
+            setPoint( "u1",   w * 0.7, h * 1.1, - 1 );
+            setPoint( "u2", - w * 0.7, h * 1.1, - 1 );
+            setPoint( "u3",         0, h * 2,   - 1 )
+            geometry.getAttribute( 'position' ).needsUpdate = true;
+
+        };
+
+    }();
+
+
+
+
+
+
+    /**
 	 * @author alteredq / http://alteredqualia.com/
 	 * @author Mugen87 / https://github.com/Mugen87
 	 *
@@ -40839,10 +40998,10 @@
 
 		// far
 
-		addLine( "f1", "f2", colorFrustum );
-		addLine( "f2", "f4", colorFrustum );
-		addLine( "f4", "f3", colorFrustum );
-		addLine( "f3", "f1", colorFrustum );
+		//addLine( "f1", "f2", colorFrustum );
+		//addLine( "f2", "f4", colorFrustum );
+		//addLine( "f4", "f3", colorFrustum );
+		//addLine( "f3", "f1", colorFrustum );
 
 		// sides
 
@@ -43047,6 +43206,7 @@
 	exports.FaceNormalsHelper = FaceNormalsHelper;
 	exports.DirectionalLightHelper = DirectionalLightHelper;
 	exports.CameraHelper = CameraHelper;
+    exports.CameraViz = CameraViz;
 	exports.BoxHelper = BoxHelper;
 	exports.ArrowHelper = ArrowHelper;
 	exports.AxisHelper = AxisHelper;
