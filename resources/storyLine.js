b4cdfd3b6df473ed00d78f68fb99ed8ff72220e3
Hannah Pinson <pinsonhannah@gmail.com>
2017-05-17 10:15:08 +0200

    storyline color change for clicking and narrative

--- resources/storyLine.js
+++ resources/storyLine.js
@@ -1,3 +1,19 @@
+var setStoryLineColors = function(clickedValue){
+  var completedColor = "gray";
+
+  console.log("setting story line colors");
+
+  rect.style("fill", function(d){if(d.value <= clickedValue){return completedColor} return "white"})
+  rect.style("stroke", function(d){if(d.value < clickedValue){return completedColor} return "white"} )
+  lines.style("stroke", function(d){if(d.value < clickedValue){return completedColor} return "white"} )
+
+};
+
+var rect;
+var lines;
+
+
+
 var StoryLine = {
   draw: function(id, events, options){
     var cfg = {
@@ -40,135 +56,136 @@ var StoryLine = {
     //Calculate times in terms of timestamps
     if(!cfg.dateDimension){
       var timestamps = events.map(function(d){return  d.value});//new Date(d.date).getTime()});
-      var maxValue = d3.max(timestamps);
-      var minValue = d3.min(timestamps);
-    }else{
+var maxValue = d3.max(timestamps);
+var minValue = d3.min(timestamps);
+}else{
       var timestamps = events.map(function(d){return  Date.parse(d.date);});//new Date(d.date).getTime()});
-      var maxValue = d3.max(timestamps);
-      var minValue = d3.min(timestamps);
+var maxValue = d3.max(timestamps);
+var minValue = d3.min(timestamps);
+}
+var margin = (d3.max(events.map(function(d){return d.radius})) || cfg.radius)*1.5+cfg.lineWidth;
+var step = (cfg.horizontalLayout)?((cfg.width-2*margin)/(maxValue - minValue)):((cfg.height-2*margin)/(maxValue - minValue));
+var series = [];
+if(maxValue == minValue){step = 0;if(cfg.horizontalLayout){margin=cfg.width/2}else{margin=cfg.height/2}}
+
+linePrevious = {
+  x1 : null,
+  x2 : null,
+  y1 : null,
+  y2 : null
+}
+
+lines = svg.selectAll("line")
+.data(events).enter().append("line")
+.attr("class", "timeline-line")
+.attr("x1", function(d){
+  var ret;
+  if(cfg.horizontalLayout){
+    var datum = (cfg.dateDimension)?new Date(d.date).getTime():d.value;
+    ret = Math.floor(step*(datum - minValue) + margin)
+  }
+  else{
+    ret = Math.floor(cfg.width/2)
+  }
+  linePrevious.x1 = ret
+  return ret
+})
+.attr("x2", function(d){
+  if (linePrevious.x1 != null){
+    return linePrevious.x1
+  }
+  if(cfg.horizontalLayout){
+    var datum = (cfg.dateDimension)?new Date(d.date).getTime():d.value;
+    ret = Math.floor(step*(datum - minValue ))
+  }
+  return Math.floor(cfg.width/2)
+})
+.attr("y1", function(d){
+  var ret;
+  if(cfg.horizontalLayout){
+    ret = Math.floor(cfg.height/2)
+  }
+  else{
+    var datum = (cfg.dateDimension)?new Date(d.date).getTime():d.value;
+    ret = Math.floor(step*(datum - minValue)) + margin
+  }
+  linePrevious.y1 = ret
+  return ret
+})
+.attr("y2", function(d){
+  if (linePrevious.y1 != null){
+    return linePrevious.y1
+  }
+  if(cfg.horizontalLayout){
+    return Math.floor(cfg.height/2)
+  }
+  var datum = (cfg.dateDimension)?new Date(d.date).getTime():d.value;
+  return Math.floor(step*(datum - minValue))
+})
+.style("stroke", function(d){
+  if(d.color != undefined){
+    return d.color
+  }
+  if(d.series != undefined){
+    if(series.indexOf(d.series) < 0){
+      series.push(d.series);
     }
-    var margin = (d3.max(events.map(function(d){return d.radius})) || cfg.radius)*1.5+cfg.lineWidth;
-    var step = (cfg.horizontalLayout)?((cfg.width-2*margin)/(maxValue - minValue)):((cfg.height-2*margin)/(maxValue - minValue));
-    var series = [];
-    if(maxValue == minValue){step = 0;if(cfg.horizontalLayout){margin=cfg.width/2}else{margin=cfg.height/2}}
+    return cfg.seriesColor(series.indexOf(d.series));
+  }
+  return cfg.color})
+.style("stroke-width", cfg.lineWidth);
 
-    linePrevious = {
-      x1 : null,
-      x2 : null,
-      y1 : null,
-      y2 : null
+rect = svg.selectAll("rect")
+.data(events).enter()
+.append("rect")
+.attr("class", "timeline-event")
+.attr("width", function(d){if(d.radius != undefined){return d.radius} return cfg.radius})
+.attr("height", function(d){if(d.radius != undefined){return d.radius} return cfg.radius  })
+.style("stroke", function(d){
+  if(d.color != undefined){
+    return d.color
+  }
+  if(d.series != undefined){
+    if(series.indexOf(d.series) < 0){
+      series.push(d.series);
     }
-
-    svg.selectAll("line")
-    .data(events).enter().append("line")
-    .attr("class", "timeline-line")
-      .attr("x1", function(d){
-                      var ret;
-                      if(cfg.horizontalLayout){
-                        var datum = (cfg.dateDimension)?new Date(d.date).getTime():d.value;
-                        ret = Math.floor(step*(datum - minValue) + margin)
-                      }
-                      else{
-                        ret = Math.floor(cfg.width/2)
-                      }
-                      linePrevious.x1 = ret
-                      return ret
-                      })
-    .attr("x2", function(d){
-                      if (linePrevious.x1 != null){
-                          return linePrevious.x1
-                      }
-                      if(cfg.horizontalLayout){
-                        var datum = (cfg.dateDimension)?new Date(d.date).getTime():d.value;
-                        ret = Math.floor(step*(datum - minValue ))
-                      }
-                      return Math.floor(cfg.width/2)
-                      })
-    .attr("y1", function(d){
-                      var ret;
-                      if(cfg.horizontalLayout){
-                        ret = Math.floor(cfg.height/2)
-                      }
-                      else{
-                        var datum = (cfg.dateDimension)?new Date(d.date).getTime():d.value;
-                        ret = Math.floor(step*(datum - minValue)) + margin
-                      }
-                      linePrevious.y1 = ret
-                      return ret
-                      })
-    .attr("y2", function(d){
-                      if (linePrevious.y1 != null){
-                        return linePrevious.y1
-                      }
-                      if(cfg.horizontalLayout){
-                        return Math.floor(cfg.height/2)
-                      }
-                      var datum = (cfg.dateDimension)?new Date(d.date).getTime():d.value;
-                      return Math.floor(step*(datum - minValue))
-                      })
-    .style("stroke", function(d){
-                      if(d.color != undefined){
-                        return d.color
-                      }
-                      if(d.series != undefined){
-                        if(series.indexOf(d.series) < 0){
-                          series.push(d.series);
-                        }
-                        return cfg.seriesColor(series.indexOf(d.series));
-                      }
-                      return cfg.color})
-    .style("stroke-width", cfg.lineWidth);
-
-    svg.selectAll("rect")
-    .data(events).enter()
-    .append("rect")
-    .attr("class", "timeline-event")
-    .attr("width", function(d){if(d.radius != undefined){return d.radius} return cfg.radius})
-    .attr("height", function(d){if(d.radius != undefined){return d.radius} return cfg.radius  })
-    .style("stroke", function(d){
-                    if(d.color != undefined){
-                      return d.color
-                    }
-                    if(d.series != undefined){
-                      if(series.indexOf(d.series) < 0){
-                        series.push(d.series);
-                      }
-                      console.log(d.series, series, series.indexOf(d.series));
-                      return cfg.seriesColor(series.indexOf(d.series));
-                    }
-                    return cfg.color}
-    )
-    .style("stroke-width", function(d){if(d.lineWidth != undefined){return d.lineWidth} return cfg.lineWidth})
-    .style("fill", function(d){if(d.background != undefined){return d.background} return cfg.background})
-    .attr("y", function(d){
-        if(cfg.horizontalLayout){
-          if (d.radius)
-            return Math.floor(cfg.height/2) - d.radius / 2
-          else 
-            return Math.floor(cfg.height/2) - cfg.radius / 2
-        }
-        var datum = (cfg.dateDimension)?new Date(d.date).getTime():d.value;
-        return Math.floor(step*(datum - minValue) + margin)
-    })
-    .attr("x", function(d){
-        if(cfg.horizontalLayout){
-          var datum = (cfg.dateDimension)?new Date(d.date).getTime():d.value;
-          var x=  Math.floor(step*(datum - minValue) + margin);
-          return x;
-        }
-        return Math.floor(cfg.width/2)
-    }).on("mouseover", function(d){
-      if(cfg.dateDimension){
-        var format = d3.time.format(cfg.dateFormat);
-        var datetime = format(new Date(d.date));
-        var dateValue = (datetime != "")?(d.name +" <small>("+datetime+")</small>"):d.name;
-      }else{
+    console.log(d.series, series, series.indexOf(d.series));
+    return cfg.seriesColor(series.indexOf(d.series));
+  }
+  return cfg.color}
+  )
+.style("stroke-width", function(d){if(d.lineWidth != undefined){return d.lineWidth} return cfg.lineWidth})
+.style("fill", function(d){if(d.background != undefined){return d.background} return cfg.background})
+.attr("y", function(d){
+  if(cfg.horizontalLayout){
+    if (d.radius)
+      return Math.floor(cfg.height/2) - d.radius / 2
+    else 
+      return Math.floor(cfg.height/2) - cfg.radius / 2
+  }
+  var datum = (cfg.dateDimension)?new Date(d.date).getTime():d.value;
+  return Math.floor(step*(datum - minValue) + margin)
+})
+.attr("x", function(d){
+  if(cfg.horizontalLayout){
+    var datum = (cfg.dateDimension)?new Date(d.date).getTime():d.value;
+    var x=  Math.floor(step*(datum - minValue) + margin);
+    return x;
+  }
+  return Math.floor(cfg.width/2)
+}).on("mouseover", function(d){
+  if(cfg.dateDimension){
+    var format = d3.time.format(cfg.dateFormat);
+    var datetime = format(new Date(d.date));
+    var dateValue = (datetime != "")?(d.name +" <small>("+datetime+")</small>"):d.name;
+  }else{
         var format = function(d){return d}; // TODO
         var datetime = d.value;
         var dateValue = d.name +" <small>("+d.value+")</small>";
       }
       d3.select(this)
-      .style("fill", function(d){if(d.color != undefined){return d.color} return cfg.color}).transition()
+      //.style("fill", function(d){if(d.color != undefined){return d.color} return cfg.color})
+      .transition()
       .duration(100).attr("r",  function(d){if(d.radius != undefined){return Math.floor(d.radius*1.5)} return Math.floor(cfg.radius*1.5)});
       tip.html("");
 
@@ -182,16 +199,18 @@ var StoryLine = {
       .style("opacity", .9);
 
     })
-    .on("mouseout", function(){
-        d3.select(this)
-        .style("fill", function(d){if(d.background != undefined){return d.background} return cfg.background}).transition()
-        .duration(100).attr("r", function(d){if(d.radius != undefined){return d.radius} return cfg.radius});
-        tip.transition()
-        .duration(100)
-    .style("opacity", 0)})
-    .on("click", function(d){
-      render(d.name);
-    });
+.on("mouseout", function(){
+  d3.select(this)
+  //.style("fill", function(d){if(d.background != undefined){return d.background} return cfg.background})
+  .transition()
+  .duration(100).attr("r", function(d){if(d.radius != undefined){return d.radius} return cfg.radius});
+  tip.transition()
+  .duration(100)
+  .style("opacity", 0)})
+.on("click", function(d){
+  setStoryLineColors(d.value);
+  render(d.name);
+});
 
     /*//Adding start and end labels
     if(cfg.showLabels != false){
@@ -213,13 +232,13 @@ var StoryLine = {
          .text(endString).style("font-size", "70%").style("fill", "white")
          .attr("x", function(d){if(cfg.horizontalLayout){return  cfg.width -  d3.max([this.getBBox().width, (margin+this.getBBox().width/2)])} return Math.floor(this.getBBox().width/2)})
          .attr("y", function(d){if(cfg.horizontalLayout){return Math.floor(cfg.height/2+(margin+this.getBBox().height))}return cfg.height-margin+this.getBBox().height/2})
-    }*/
+       }*/
 
 
-    svg.on("mousemove", function(){
+       svg.on("mousemove", function(){
         tipPixels = parseInt(tip.style("height").replace("px", ""));
-    return tip.style("top", (d3.event.pageY-tipPixels-margin)+"px").style("left",(d3.event.pageX+20)+"px");})
-    .on("mouseout", function(){return tip.style("opacity", 0).style("top","0px").style("left","0px");});
-  }
-}
+        return tip.style("top", (d3.event.pageY-tipPixels-margin)+"px").style("left",(d3.event.pageX+20)+"px");})
+       .on("mouseout", function(){return tip.style("opacity", 0).style("top","0px").style("left","0px");});
+     }
+   }
 
